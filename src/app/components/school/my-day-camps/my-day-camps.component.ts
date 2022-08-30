d6091f96c50ef33fb078671d254514e840bf3f75
mreda <reda.mateusz@gmail.com>
2022-08-30 22:16:38 +0200

    For schools -> My day camps - Add day camp functionality

--- src/app/components/school/my-day-camps/my-day-camps.component.ts
+++ src/app/components/school/my-day-camps/my-day-camps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {DayCamp} from "../../../model/interfaces/day-camp";
 import {SchoolDayCampsService} from "../../../services/school-day-camps.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-my-day-camps',
@@ -11,10 +13,60 @@ export class MyDayCampsComponent implements OnInit {
   public futureDayCamps?: DayCamp[];
   public ongoingDayCamps?: DayCamp[];
   public pastDayCamps?: DayCamp[];
+  public addDayCampForm!: FormGroup;
 
-  constructor(private schoolDayCampsService: SchoolDayCampsService) { }
+  constructor(
+    private schoolDayCampsService: SchoolDayCampsService,
+    private modalService: NgbModal
+    ) { }
 
   ngOnInit(): void {
+    this.addDayCampForm = new FormGroup({
+      "name": new FormControl(null, [Validators.required]),
+      "description": new FormControl(null, [Validators.required]),
+      "startDate": new FormControl(null, [Validators.required, MyDayCampsComponent.startDateMustBeFutureValidator]),
+      "endDate": new FormControl(null, [Validators.required]),
+      "price": new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]+(\.[0-9][0-9]?)?$/)]),
+      "capacity": new FormControl(null, [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    },{validators:MyDayCampsComponent.endDateNotBeforeStartDateValidator})
+
+    this.getDayCamps();
+  }
+
+  private static startDateMustBeFutureValidator(control: AbstractControl): ValidationErrors | null {
+    const startDate = new Date(control.value);
+    const today = new Date();
+
+    const isDateValid: boolean = startDate > today;
+    return isDateValid ? null : {startDateBeforeTomorrow: control.value}
+  }
+
+  private static endDateNotBeforeStartDateValidator(control: AbstractControl): ValidationErrors | null {
+    const startDate = control.get('startDate')?.value;
+    const endDate = control.get('endDate')?.value;
+
+    const isDateValid = new Date(endDate) >= new Date(startDate);
+    return isDateValid ? null : {endDateBeforeStartDate: endDate}
+  }
+
+  openAddDayCampModal(modal: any) {
+    this.modalService.open(modal, {ariaLabelledBy: 'modal-basic-title'}).result.then(
+      result => this.addDayCampForm.reset(),
+      reason => this.addDayCampForm.reset()
+    )
+  }
+
+  onAddDayCampSubmit() {
+    this.schoolDayCampsService.addDayCamp(this.addDayCampForm).subscribe({
+      next: value => this.getDayCamps(),
+      error: err => {
+        console.log(err);
+      }
+    });
+    this.addDayCampForm.reset();
+  }
+
+  private getDayCamps(): void {
     this.schoolDayCampsService.getFutureDayCamps().subscribe({
       next: value => {
         this.futureDayCamps = value.dayCamps;
--- src/app/services/school-day-camps.service.ts
+++ src/app/services/school-day-camps.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DayCampsResponse} from "../model/interfaces/day-camps-response";
+import {FormGroup} from "@angular/forms";
 
 const SCHOOL_DAY_CAMPS_URL = 'http://localhost:8080/api/school/daycamps'
 const TIMELINE_LOCATION_QUERY_PARAM = '?timelineLocation='
 const PAST_SUFFIX = 'PAST'
 const ONGOING_SUFFIX = 'ONGOING'
 const FUTURE_SUFFIX = 'FUTURE'
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
@@ -32,4 +37,14 @@ export class SchoolDayCampsService {
       {responseType: 'json'});
   }
 
+  addDayCamp(addDayCampForm: FormGroup): Observable<any> {
+    return this.httpClient.post(SCHOOL_DAY_CAMPS_URL, {
+      name: addDayCampForm.value.name,
+      description: addDayCampForm.value.description,
+      startDate: addDayCampForm.value.startDate,
+      endDate: addDayCampForm.value.endDate,
+      price: addDayCampForm.value.price,
+      capacity: addDayCampForm.value.capacity
+    }, httpOptions);
+  }
 }
